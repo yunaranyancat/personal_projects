8037fab87d1854bf4de885c8c0e84a207eefd75e
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2017-12-07 19:01:36 +0100

    edit summary in copySnakev1.py

--- project_1/copySnakev1.py
+++ project_1/copySnakev1.py
@@ -5,7 +5,7 @@ Requirement : pynput external module (https://github.com/moses-palmer/pynput)
 Basic python keylogger(version 1): https://www.youtube.com/watch?v=x8GbWt56TlY
 
 This is for educational purpose only,
-please don't misuse for malicious/illegal purpose!
+please don't misuse for malicious/illegal purposes!
 
 DISCLAIMER :The creator of this script take no responsibility for any purposes regarding the usage of the script.
 """
