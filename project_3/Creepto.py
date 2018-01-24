1e6432d5dde9b5f6146b6acd0be26f1724aa2cc0
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-01-24 22:21:37 +0100

    playing with the methods in the library

--- project_3/Creepto.py
+++ project_3/Creepto.py
@@ -1,4 +1,18 @@
 #EXAMPLE TESTING
 from Crypto.Hash import SHA256
+from Crypto.Cipher import AES
+from Crypto.Random import random
+
+
 hash = SHA256.new().hexdigest()
-print(hash)
+#print(hash)
+key = 'ABCDEFGHIJKLM123'
+iv = 'IVIVIVIVIVIVIVIV'
+obj = AES.new(key,AES.MODE_CBC,iv)
+message = "The answer is no" #fixed data block size of 16 bytes
+ciphertext = obj.encrypt(message)
+obj2 = AES.new(key, AES.MODE_CBC,iv)
+print(obj2.decrypt(ciphertext))
+
+value = random.choice(['dogs','doggo','doge'])
+print(value)
--- project_3/README.md
+++ project_3/README.md
@@ -1,7 +1,12 @@
 In this project, I will create a program based on a Python library, called pycrypto..
 
-The name of the program will be creepto(subject to be changed in the future)
+The name of the program will be creepto(subject to be changed in the future,but,creepto, why not?)
+
 Pre requirements:
 pycrypto library (install in Ubuntu by using pip3 install pycrypto)
+
+Resources:
+https://pypi.python.org/pypi/pycrypto
+https://www.dlitz.net/software/pycrypto/
 
 Incoming interesting ideas..
