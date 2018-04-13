03f95696ec0fa3ecf4b4b25b87891caf5743b77d
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-04-13 21:28:37 +0800

    Regex

---
+++ project_5/regtest.py
@@ -0,0 +1,19 @@
+import re
+
+def main():
+    line = "I think I understand regular expressions"
+
+    matchResult = re.match('think', line , re.M|re.I)
+    if matchResult:
+        print("Match Found: "+matchResult.group())
+    else:
+        print("No Match was Found")
+
+    searchResult = re.search('think', line, re.M|re.I)
+    if searchResult:
+        print("Search Found: "+searchResult.group())
+    else:
+        print("Nothing found in search")
+
+if __name__ == "__main__":
+    main()
