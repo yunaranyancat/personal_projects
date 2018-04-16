b8aecb3818e08a0f42492468d0855514f987e174
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-04-16 21:39:47 +0800

    CGI(common graphical interface) in Python3

---
+++ project_5/hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+print("Content-type:text/html\r\n\r\n")
+print("<html><body>")
+print("<h1> It Works! </h1>")
+for i in range(5):
+    print("<h2> Hello World! "+str(i)+"</h2>")
+print("</body></html>")
