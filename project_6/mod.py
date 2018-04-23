dd871883357ff19d1f2b3af0a6aa63ecb39cea51
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-04-23 11:55:34 +0800

    Modulating Codes in Python3

---
+++ project_6/mod.py
@@ -0,0 +1,14 @@
+#import vector2dv2 as vector2d
+from vector2dv2 import Vector2D #or *
+
+def main():
+    vec1 = Vector2D(5, 6)
+    vec2 = Vector2D(1, 1)
+
+    print(vec1.x)
+    print(vec1.y)
+    print(vec2.x)
+    print(vec2.y)
+
+if __name__ == '__main__':
+    main()
