3550150e375d99756158ac7193c58e092e0411c4
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-04-24 19:57:47 +0800

    v1.1

--- project_7/caesarCipher.py
+++ project_7/caesarCipher.py
@@ -1,25 +1,68 @@
 import argparse
 
-def extract_line(filename):
-    with open(filename, "r+") as thefile:
+def caesar_cipher(filename_to_read,filename_to_append,mode):
+    with open(filename_to_read, "r+") as thefile:
         lines = thefile.readlines()
-        #print(lines)
-        for each_line in lines:
-            strip_line = each_line.strip('\n')
-            if (strip_line != ''):
-                for each_char_inline in strip_line:
-                    #.....
+        if (mode=="encrypt"):
+            with open(filename_to_append, 'w+') as theEncryptedFile:
+                for each_line in lines:
+                    encrypted_text = encrypt_text(each_line)
+                    theEncryptedFile.write(encrypted_text)
 
+        elif (mode=="decrypt"):
+            with open(filename_to_append, 'w+') as theDecryptedFile:
+                for each_line in lines:
+                    decrypted_text = decrypt_text(each_line)
+                    theDecryptedFile.write(decrypted_text)
+
+
+def encrypt_text(text):
+    words = ''
+    for each_line in text:
+        if (32 < ord(each_line) < 123):
+            words += chr(ord(each_line)+3)
+        else:
+            words += each_line
+    return words
+
+def decrypt_text(text):
+    words = ''
+    for each_line in text:
+        if (32 < (ord(each_line)-3) < 123):
+            words += chr(ord(each_line)-3)
+        else:
+            words += each_line
+    return words
 
 def main():
-    # print("hey, welcome to my version of Caesar cipher"+'\n')
-    # parser = argparse.ArgumentParser()
-    # group = parser.add_mutually_exclusive_group()
-    # group.add_argument("-d", "--decrypt", action="store_true")
-    # group.add_argument("-e", "--encrypt", action="store_true")
-    # parser.add_argument("-f", "--file")
-    getFile = str(input("Please insert the name of your file : "))
-    extract_line(getFile)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="The file you would like to encrypt/decrypt.", type=str)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-d", "--decrypt", action="store_true")
+    group.add_argument("-e", "--encrypt", action="store_true")
+    parser.add_argument("-r", "--result", help="Specify filename for the encryption/decryption",type=str)
+
+    args = parser.parse_args()
+
+    #initialization
+    getFileName = args.filename
+    getResultFile = args.filename
+    mode = "NOMODE"
+
+
+    if args.result:
+        getResultFile = args.result
+
+    if not(args.decrypt or args.encrypt):
+        parser.error("Please select one of the options, -e for encryption, -d for decryption")
+    else:
+        if args.decrypt:
+            mode = "decrypt"
+            caesar_cipher(getFileName,getResultFile,mode)
+        elif args.encrypt:
+            mode = "encrypt"
+            caesar_cipher(getFileName,getResultFile,mode)
+
 
 if __name__ == '__main__':
     main()
